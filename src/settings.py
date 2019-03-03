a0ed9e39d80d8f65a69915fb82017946886abf97
Dawid Skreczko <dawid.skreczko@gmail.com>
2019-03-03 18:34:18 +0100

    problems with static v2

--- src/settings.py
+++ src/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = config('DEBUG', default = False, cast= bool)
 
 ALLOWED_HOSTS = ['exam-sheet.herokuapp.com', 'localhost:8000', '127.0.0.1']
 
@@ -126,9 +126,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 
 from src.restconf.main import *
