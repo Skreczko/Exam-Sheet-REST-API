ed904aad576b41fa39d04e1844adb29d6300c6d7
Dawid Skreczko <dawid.skreczko@gmail.com>
2019-03-03 15:45:24 +0100

    all files to heroku v13

--- src/settings.py
+++ src/settings.py
@@ -127,11 +127,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR)
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR),
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
 
 
 from src.restconf.main import *
