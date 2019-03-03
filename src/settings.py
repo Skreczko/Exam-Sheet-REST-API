b1350aa0242223f869678f6b03040db675b607cc
Dawid Skreczko <dawid.skreczko@gmail.com>
2019-03-03 14:58:08 +0100

    adding to heroku

--- src/settings.py
+++ src/settings.py
@@ -23,12 +23,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
-SECRET_KEY = ''
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['exam-sheet.herokuapp.com', 'localhost:8000']
 
 
 # Application definition
